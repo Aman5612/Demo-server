113e4cf66433bf16df5099473cd61789f1147f00
Aman Tiwari <amankumartiwari392@gmail.com>
2024-06-06 13:51:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*", credentials: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  try {
+    const { email } = req.body;
+
+    console.log("Received email:", req.body);
+    res.send("Email sent to " + email);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/", (re, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3002, () => {
+  console.log("Server is running on port 3002");
+});
